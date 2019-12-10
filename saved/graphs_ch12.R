2e18bcf46a658b3184009267975779f18cf3d716
juliosolisar <juliosolisar@hotmail.com>
2019-12-10 20:35:50 +0100

    Update graphs_ch12.R

--- saved/graphs_ch12.R
+++ saved/graphs_ch12.R
@@ -1,99 +1,92 @@
-library(dplyr)
-library(ggplot2)
-<<<<<<< Updated upstream
-
-tograph <- Ch12_chain_model_regr$diagnoses_df %>%
-=======
 library(tidyverse)
 
+#Ch12_chain_model_regr <- readRDS("saved/Ch12_chain_model_regr.rds")
+#Ch12_chain_model_suff <- readRDS("saved/Ch12_chain_model_suff.rds")
+#Ch12_chain_model_conc <- readRDS("saved/Ch12_chain_model_conc.rds")
+#Ch12_chain_model_flat <- readRDS("saved/Ch12_chain_model_flat.rds")
+#Ch12_chain_model_necs <- readRDS("saved/Ch12_chain_model_necs.rds")
 
-given <-  Ch12_chain_model_regr$diagnoses_df %>%
->>>>>>> Stashed changes
-  mutate(Wide=substr(strategy, 3, 3)) %>%
-  mutate(Deep= as.numeric(substr(strategy, 7, 7))) %>%
+#givens <- list(Ch12_chain_model_regr,
+#               Ch12_chain_model_suff,
+#               Ch12_chain_model_conc,
+#               Ch12_chain_model_flat,
+#               Ch12_chain_model_necs)
+
+#process <- function(given) {
+#  given <<- given %>%
+#    dplyr::mutate(Deep = as.numeric(sub(".*_d_", "", strategy))) %>%
+#    dplyr::mutate(Wide = as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
+#    dplyr::filter(!is.na(Deep)) %>%
+#    dplyr::mutate(upper = post_var + post_var_sd) %>%
+#    dplyr::mutate(lower = post_var - post_var_sd)
+#}
+
+#lapply(givens, process)
+
+given1 <- readRDS("saved/Ch12_chain_model_regr.rds")$diagnoses_df %>%
+  mutate(Deep= as.numeric(sub(".*_d_", "", strategy))) %>%
+  mutate(Wide= as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
   filter(!is.na(Deep)) %>%
-  mutate(upper= post_var + post_var_sd) %>%
-<<<<<<< Updated upstream
-  mutate(lower= post_var - post_var_sd)
+  mutate(upper = post_var + post_var_sd) %>%
+  mutate(lower = post_var - post_var_sd) %>%
+  mutate(given = "reg")
 
-wide.labs <- c("Deep = 0", "Deep = 2", "Deep = 4")
-names(wide.labs) <- c("0", "2", "4")
+given2 <- readRDS("saved/Ch12_chain_model_suff.rds")$diagnoses_df %>%
+  mutate(Deep= as.numeric(sub(".*_d_", "", strategy))) %>%
+  mutate(Wide= as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
+  filter(!is.na(Deep)) %>%
+  mutate(upper = post_var + post_var_sd) %>%
+  mutate(lower = post_var - post_var_sd) %>%
+  mutate(given = "suff")
 
-ggplot(dplyr::filter(tograph, Query=="ATE"), aes(x= Deep, y=post_var)) + 
-  geom_point(shape = 15, size  = 2) + 
-  facet_wrap(~Wide) +
-=======
-  mutate(lower= post_var - post_var_sd) 
+given3 <- readRDS("saved/Ch12_chain_model_conc.rds")$diagnoses_df %>%
+  mutate(Deep= as.numeric(sub(".*_d_", "", strategy))) %>%
+  mutate(Wide= as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
+  filter(!is.na(Deep)) %>%
+  mutate(upper = post_var + post_var_sd) %>%
+  mutate(lower = post_var - post_var_sd) %>%
+  mutate(given = "conc")
 
-given1 <- given%>%
-  mutate(given="reg")
+given4 <- readRDS("saved/Ch12_chain_model_flat.rds")$diagnoses_df %>%
+  mutate(Deep= as.numeric(sub(".*_d_", "", strategy))) %>%
+  mutate(Wide= as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
+  filter(!is.na(Deep)) %>%
+  mutate(upper = post_var + post_var_sd) %>%
+  mutate(lower = post_var - post_var_sd) %>%
+  mutate(given = "flat")
 
-given2 <- given %>%
-  mutate(given="flat")
-
-given3 <- given %>%
-  mutate(given="suff")
-
-given4 <- given %>%
-  mutate(given="necs")
-
-given5 <- given %>%
-  mutate(given="conc")
+given5 <- readRDS("saved/Ch12_chain_model_necs.rds")$diagnoses_df %>%
+  mutate(Deep= as.numeric(sub(".*_d_", "", strategy))) %>%
+  mutate(Wide= as.numeric(sub(".*w[_]([^.]+)[_]d.*", "\\1", strategy))) %>%
+  filter(!is.na(Deep)) %>%
+  mutate(upper = post_var + post_var_sd) %>%
+  mutate(lower = post_var - post_var_sd) %>%
+  mutate(given = "necs")
 
 tograph <- bind_rows(given1, given2, given3, given4, given5)
 
-ggplot(dplyr::filter(tograph, Query=="ATE"), aes(x= Deep, y=post_var)) + 
-  geom_point(shape = 15, size  = 2) + 
+ggplot(dplyr::filter(tograph, Query=="ATE"), aes(x= Deep, y=post_var)) +
+  geom_point(shape = 15, size  = 2) +
   facet_grid(vars(given), vars(Wide)) +
->>>>>>> Stashed changes
-  scale_x_continuous(breaks=seq(0, 4, 2)) + 
-  expand_limits(y=.025) +
   geom_errorbar(aes(ymin  = lower, ymax  = upper, width = 0.15))+
   theme_bw() +
-<<<<<<< Updated upstream
-  ggtitle("Wide and Deep Stategies fo a Chain Model. Given = Reg; Query = ATE ")
-=======
   ylab("Expected Posterior Variance") +
   ggtitle("Wide and Deep Stategies for a Chain Model. Query = ATE ")
->>>>>>> Stashed changes
 
 
-ggplot(dplyr::filter(tograph, Query=="ProbPos"), aes(x= Deep, y=post_var)) + 
-  geom_point(shape = 15, size  = 2) + 
-<<<<<<< Updated upstream
-  facet_wrap(~Wide) +
-=======
+ggplot(dplyr::filter(tograph, Query=="ProbPos"), aes(x= Deep, y=post_var)) +
+  geom_point(shape = 15, size  = 2) +
   facet_grid(vars(given), vars(Wide)) +
->>>>>>> Stashed changes
-  scale_x_continuous(breaks=seq(0, 4, 2)) + 
-  expand_limits(y=c(.035, .055)) +
   geom_errorbar(aes(ymin  = lower, ymax  = upper, width = 0.15))+
   theme_bw() +
   ylab("Expected Posterior Variance") +
-<<<<<<< Updated upstream
-  ggtitle("Wide and Deep Stategies fo a Chain Model. Given = Reg; Query = ProbPos ")
+  ggtitle("Wide and Deep Stategies for a Chain Model. Query = ProbPos")
 
-ggplot(dplyr::filter(tograph, Query=="via_M"), aes(x= Deep, y=post_var)) + 
-  geom_point(shape = 15, size  = 2) + 
-  facet_wrap(~Wide) +
-=======
-  ggtitle("Wide and Deep Stategies for a Chain Model. Query = ProbPos ")
 
-ggplot(dplyr::filter(tograph, Query=="via_M"), aes(x= Deep, y=post_var)) + 
-  geom_point(shape = 15, size  = 2) + 
+ggplot(dplyr::filter(tograph, Query=="via_M"), aes(x= Deep, y=post_var)) +
+  geom_point(shape = 15, size  = 2) +
   facet_grid(vars(given), vars(Wide)) +
-  ylab("Expected Posterior Variance") +
->>>>>>> Stashed changes
-  scale_x_continuous(breaks=seq(0, 4, 2)) +
-  scale_y_continuous(breaks=seq(.0, .08, .015))  +
   geom_errorbar(aes(ymin  = lower, ymax  = upper, width = 0.15))+
   theme_bw() +
   ylab("Expected Posterior Variance") +
-<<<<<<< Updated upstream
-  ggtitle("Wide and Deep Stategies fo a Chain Model. Given = Reg; Query = via_M ")
-=======
-  ggtitle("Wide and Deep Stategies for a Chain Model. Query = via_M ")
->>>>>>> Stashed changes
-
-
-  
+  ggtitle("Wide and Deep Stategies for a Chain Model. Query = via_M")
